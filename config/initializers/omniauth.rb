5f88154bb74e37fea06fe33344e61eb033bab868
marsz <marsz330@gmail.com>
2012-12-06 04:42:34 +0800

    fix omniauth config

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,5 +1,4 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
-  $Omniauth_Config = ActiveSupport::HashWithIndifferentAccess.new(YAML.load(File.open("#{Rails.root}/config/omniauth.yml"))[Rails.env])
   # perms = 'email,offline_access,user_checkins,friends_checkins,user_location'
   provider :facebook, Setting.facebook_app_id, Setting.facebook_secret, :scope => Setting.facebook_perms
 end
