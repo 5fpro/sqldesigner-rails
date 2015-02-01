158c817b618e3df46aef0bdf9f2c7c90e5c8330a
marsz <marsz330@gmail.com>
2015-02-01 17:52:22 +0800

    admin user

--- app/models/user.rb
+++ app/models/user.rb
@@ -20,6 +20,7 @@
 #  unconfirmed_email      :string
 #  created_at             :datetime
 #  updated_at             :datetime
+#  admin                  :boolean          default(FALSE)
 #
 
 class User < ActiveRecord::Base
