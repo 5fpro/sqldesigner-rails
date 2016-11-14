307624d703c49809c08b19f9f82c1ee0e54ae49a
ayaya <ayaya@ayaya.tw>
2016-11-14 16:54:26 +0800

    update config

--- rails4/config/routes.rb
+++ rails4/config/routes.rb
@@ -1,20 +1,20 @@
 require 'sidekiq/web'
-require 'sidetiq/web'
+
 Rails.application.routes.draw do
   mount Sidekiq::Web => '/sidekiq'
 
   devise_for :users
-  get "/authorizations/:provider/callback", to: "authorizations#callback"
-  get "/authorizations/failure" => "authorizations#failue", as: :auth_failure
+  get '/authorizations/:provider/callback', to: 'authorizations#callback'
+  get '/authorizations/failure' => 'authorizations#failue', as: :auth_failure
   Setting.omniauth.providers.keys.each do |provider|
     get "/authorizations/#{provider}", as: "auth_#{provider}"
   end
 
-  root to: "base#index"
-  get '/robots.txt', to: "base#robots", defaults: { format: "text" }
+  root to: 'base#index'
+  get '/robots.txt', to: 'base#robots', defaults: { format: 'text' }
 
   namespace :admin do
-    root to: "base#index"
+    root to: 'base#index'
     resources :users
     resources :categories do
       member do
