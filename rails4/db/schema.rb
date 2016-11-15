a60247ae5e951c63198c22b39088d6c922100756
ayaya <ayaya@ayaya.tw>
2016-11-15 15:46:16 +0800

    use sinatra 2.0.0.beta2

--- rails4/db/schema.rb
+++ rails4/db/schema.rb
@@ -1,4 +1,3 @@
-# encoding: UTF-8
 # This file is auto-generated from the current state of the database. Instead
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
@@ -27,11 +26,10 @@ ActiveRecord::Schema.define(version: 20151216161107) do
     t.hstore   "data"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.index ["auth_type", "auth_id", "provider"], name: "index_authorizations_on_auth_type_and_auth_id_and_provider", using: :btree
+    t.index ["auth_type", "auth_id"], name: "index_authorizations_on_auth_type_and_auth_id", using: :btree
+    t.index ["provider", "uid"], name: "index_authorizations_on_provider_and_uid", using: :btree
   end
-
-  add_index "authorizations", ["auth_type", "auth_id", "provider"], name: "index_authorizations_on_auth_type_and_auth_id_and_provider", using: :btree
-  add_index "authorizations", ["auth_type", "auth_id"], name: "index_authorizations_on_auth_type_and_auth_id", using: :btree
-  add_index "authorizations", ["provider", "uid"], name: "index_authorizations_on_provider_and_uid", using: :btree
 
   create_table "categories", force: :cascade do |t|
     t.string   "name"
@@ -39,39 +37,36 @@ ActiveRecord::Schema.define(version: 20151216161107) do
     t.datetime "updated_at", null: false
     t.datetime "deleted_at"
     t.integer  "sort"
+    t.index ["name"], name: "index_categories_on_name", using: :btree
+    t.index ["sort"], name: "index_categories_on_sort", using: :btree
   end
-
-  add_index "categories", ["name"], name: "index_categories_on_name", using: :btree
-  add_index "categories", ["sort"], name: "index_categories_on_sort", using: :btree
 
   create_table "taggings", force: :cascade do |t|
     t.integer  "tag_id"
-    t.integer  "taggable_id"
     t.string   "taggable_type"
-    t.integer  "tagger_id"
+    t.integer  "taggable_id"
     t.string   "tagger_type"
+    t.integer  "tagger_id"
     t.string   "context",       limit: 128
     t.datetime "created_at"
+    t.index ["tag_id", "taggable_id", "taggable_type", "context", "tagger_id", "tagger_type"], name: "taggings_idx", unique: true, using: :btree
+    t.index ["taggable_id", "taggable_type", "context"], name: "index_taggings_on_taggable_id_and_taggable_type_and_context", using: :btree
   end
-
-  add_index "taggings", ["tag_id", "taggable_id", "taggable_type", "context", "tagger_id", "tagger_type"], name: "taggings_idx", unique: true, using: :btree
-  add_index "taggings", ["taggable_id", "taggable_type", "context"], name: "index_taggings_on_taggable_id_and_taggable_type_and_context", using: :btree
 
   create_table "tags", force: :cascade do |t|
     t.string  "name"
     t.integer "taggings_count", default: 0
+    t.index ["name"], name: "index_tags_on_name", unique: true, using: :btree
   end
-
-  add_index "tags", ["name"], name: "index_tags_on_name", unique: true, using: :btree
 
   create_table "users", force: :cascade do |t|
     t.string   "name"
-    t.string   "email",                  default: "", null: false
-    t.string   "encrypted_password",     default: "", null: false
+    t.string   "email",                  default: "",    null: false
+    t.string   "encrypted_password",     default: "",    null: false
     t.string   "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
-    t.integer  "sign_in_count",          default: 0,  null: false
+    t.integer  "sign_in_count",          default: 0,     null: false
     t.datetime "current_sign_in_at"
     t.datetime "last_sign_in_at"
     t.string   "current_sign_in_ip"
@@ -82,14 +77,13 @@ ActiveRecord::Schema.define(version: 20151216161107) do
     t.string   "unconfirmed_email"
     t.datetime "created_at"
     t.datetime "updated_at"
-    t.boolean  "admin",                              default: false
+    t.boolean  "admin",                  default: false
     t.string   "avatar"
+    t.index ["admin"], name: "index_users_on_admin", using: :btree
+    t.index ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true, using: :btree
+    t.index ["email"], name: "index_users_on_email", unique: true, using: :btree
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
   end
-
-  add_index "users", ["admin"], :name => "index_users_on_admin"
-  add_index "users", ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true, using: :btree
-  add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
-  add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
 
   create_table "versions", force: :cascade do |t|
     t.string   "item_type",      null: false
@@ -99,8 +93,7 @@ ActiveRecord::Schema.define(version: 20151216161107) do
     t.text     "object"
     t.datetime "created_at"
     t.text     "object_changes"
+    t.index ["item_type", "item_id"], name: "index_versions_on_item_type_and_item_id", using: :btree
   end
-
-  add_index "versions", ["item_type", "item_id"], name: "index_versions_on_item_type_and_item_id", using: :btree
 
 end
