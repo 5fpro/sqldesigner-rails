b22967438eb872f109ca15cd176969f461360406
ayaya <ayaya@ayaya.tw>
2016-12-14 14:12:39 +0800

    use ActiveJob as backgrounder backend

--- config/initializers/carrierwave_backgrounder.rb
+++ config/initializers/carrierwave_backgrounder.rb
@@ -3,7 +3,8 @@
 CarrierWave::Backgrounder.configure do |c|
   # c.backend :delayed_job, queue: :carrierwave
   # c.backend :resque, queue: :carrierwave
-  c.backend :sidekiq, queue: :default
+  # c.backend :sidekiq, queue: :default
+  c.backend :active_job, queue: :default
   # c.backend :girl_friday, queue: :carrierwave
   # c.backend :sucker_punch, queue: :carrierwave
   # c.backend :qu, queue: :carrierwave
