b1090bf166a84288906e5cb3e1ff3b3e668bd792
marsz <marsz330@gmail.com>
2017-04-02 20:06:51 +0800

    default og image

--- app/helpers/meta_tag_helper.rb
+++ app/helpers/meta_tag_helper.rb
@@ -16,6 +16,7 @@ module MetaTagHelper
       admins: default_meta[:fb_admin_ids]
     }
     data[:og][:image] = data[:image] if data[:image]
+    data[:og][:image] = ActionController::Base.helpers.asset_path('icon-300px.png') unless data[:og][:image]
     set_meta_tags(data.merge(
                     reverse:   default_meta[:reverse],
                     separator: default_meta[:separator],
@@ -25,6 +26,7 @@ module MetaTagHelper
 
   def default_meta
     h = SeoSettings.defaults
+
     # h[:icon] ||= [
     #   { href: ActionController::Base.helpers.asset_path('icon-100px.png'), type: 'image/png' },
     #   { href: ActionController::Base.helpers.asset_path('icon-100px.png'), type: 'image/png', rel: 'apple-touch-icon apple-touch-icon-precomposed' }
