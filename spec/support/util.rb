96a2edb8a08df442c2c6d682bcd16a6e2937a095
marsz <marsz330@gmail.com>
2017-06-04 16:10:54 +0800

    add helper method to spec

--- spec/support/util.rb
+++ spec/support/util.rb
@@ -2,4 +2,8 @@ module Util
   def omniauth_mock(provider)
     OmniAuth.config.mock_auth[provider]
   end
+
+  def read_fixtures(filename)
+    IO.read(Rails.root.join('spec').join('fixtures').join(filename))
+  end
 end
