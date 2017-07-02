9e7c6f695cb68b931d52096b30b405ab49bcaa2e
marsz <marsz330@gmail.com>
2017-07-02 21:45:45 +0800

    use settings logic

--- app/settings/base_setting.rb
+++ app/settings/base_setting.rb
@@ -1,23 +1,11 @@
-require 'singleton'
-
-class BaseSetting
-
-  attr_reader :settings
-
-  include Singleton
+class BaseSetting < Settingslogic
 
   class << self
-    private
 
-    def settings
-      instance.settings
+    def inherited(subklass)
+      subklass.source(Rails.root.join('config', "#{subklass.to_s.underscore}.yml"))
+      subklass.namespace(Rails.env)
     end
-  end
 
-  def initialize
-    if self.class.to_s != 'base_settings'
-      @settings = Rails.application.config_for(self.class.to_s.underscore).deep_symbolize_keys
-    end
   end
-
 end
--- app/settings/seo_setting.rb
+++ app/settings/seo_setting.rb
@@ -1,7 +1,2 @@
 class SeoSetting < BaseSetting
-  class << self
-    def defaults
-      settings[:defaults]
-    end
-  end
 end
