9cc7b9dba61ef4e298f67b953bbf209909761b69
Manic Chuang <(none)>
2017-08-29 09:58:04 +0800

    feat(ErrorHandler): add rendering for other formats

--- app/controllers/errors_controller.rb
+++ app/controllers/errors_controller.rb
@@ -1,9 +1,17 @@
 class ErrorsController < ApplicationController
   def not_found
-    render(status: 404)
+    respond_to do |format|
+      format.json { render json: { message: 'Page not found' }, status: 404 }
+      format.html { render status: 404 }
+      format.any { render plain: 'Page not found', status: 404 }
+    end
   end
 
   def internal_server_error
-    render(status: 500)
+    respond_to do |format|
+      format.json { render json: { message: 'Internal Server Error' }, status: 500 }
+      format.html { render status: 500 }
+      format.any { render plain: 'Internal Server Error', status: 500 }
+    end
   end
 end
