5574985581a48fa89c37645dfddfecb7b1463a4f
marsz <marsz330@gmail.com>
2017-09-26 09:00:23 +0200

    fix(rubocop)

--- spec/turnip_helper.rb
+++ spec/turnip_helper.rb
@@ -1,6 +1,6 @@
 Turnip.type = :request
 require 'rails_helper'
-Dir[Rails.root.join('spec/steps/**/*_steps.rb')].each { |f| load f }
+Dir[Rails.root.join('spec', 'steps', '**', '*_steps.rb')].each { |f| load f }
 include FactoryGirl::Syntax::Methods
 
 RSpec.configure do |config|
