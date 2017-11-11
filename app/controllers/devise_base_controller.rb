48e9894c47ad89241662b94a5fa739265f9415ce
marsz <marsz330@gmail.com>
2017-11-11 23:35:25 +0800

    custom devise layout

--- app/controllers/devise_base_controller.rb
+++ app/controllers/devise_base_controller.rb
@@ -1,3 +1,15 @@
 class DeviseBaseController < ApplicationController
   before_action :set_meta
+
+  layout :layout_by_resource
+
+  private
+
+  def layout_by_resource
+    # if resource_name == :user
+    #
+    # else
+    'application'
+    # end
+  end
 end
