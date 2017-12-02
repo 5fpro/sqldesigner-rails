4a7c563b08bb52e57daea2e2aed57bc20569c30d
marsz <marsz330@gmail.com>
2017-12-02 13:09:17 +0800

    redactor class with js- prefix

--- app/assets/javascripts/redactor2_rails/config.js
+++ app/assets/javascripts/redactor2_rails/config.js
@@ -10,5 +10,5 @@ $(function () {
         fileUploadFields: params
     };
     // Initialize Redactor
-    $('.redactor').redactor();
+    $('.js-redactor').redactor();
 });
--- app/inputs/redactor2_input.rb
+++ app/inputs/redactor2_input.rb
@@ -3,7 +3,7 @@
 class Redactor2Input < SimpleForm::Inputs::TextInput
   def input(wrapper_options = nil)
     wrapper_options ||= {}
-    wrapper_options[:class] = "#{wrapper_options[:class]} redactor"
+    wrapper_options[:class] = "#{wrapper_options[:class]} js-redactor"
     super(wrapper_options)
   end
 end
