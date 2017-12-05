dcdcc27399b6ea04418d101206dfb1f1978f8a8b
marsz <marsz330@gmail.com>
2017-12-05 23:33:47 +0800

    fix: error mailer

--- app/mailers/error_mailer.rb
+++ app/mailers/error_mailer.rb
@@ -2,6 +2,6 @@ class ErrorMailer < BaseMailer
   default to: ENV['ERROR_MAILER_TO']
   def notify(error)
     @error = error
-    mail(subject: format_subject("Critical Error! #{@error.class}: #{@error.message}"))
+    mail(subject: "Critical Error! #{@error.class}: #{@error.message}")
   end
 end
