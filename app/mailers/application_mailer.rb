9b87a6f4f5fb619b3ec8aecc53fb6040ef8b99ec
marsz <marsz330@gmail.com>
2017-12-05 23:33:00 +0800

    feat: filtering email delivery by ENV

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -1,4 +1,5 @@
 class ApplicationMailer < ActionMailer::Base
+  include DeliverFilterConcern
   default from: Setting.mailer.default_sender
   add_template_helper MailerHelper
   layout 'mailer'
--- app/mailers/base_mailer.rb
+++ app/mailers/base_mailer.rb
@@ -1,11 +1,5 @@
 class BaseMailer < ApplicationMailer
   def notify
-    mail(to: 'hi@5fpro.com', subject: format_subject('Hello!'))
-  end
-
-  protected
-
-  def format_subject(str)
-    Rails.env.production? || Rails.env.test? ? str : "[#{Rails.env}] #{str}"
+    mail(to: 'hi@5fpro.com', subject: 'Hello!')
   end
 end
