aacec9332866f25161f2f5d02a4081fc6679c891
marsz <marsz330@gmail.com>
2017-12-11 15:57:41 +0800

    refactor: remove role_class

--- spec/steps/placeholders/role_class_steps.rb
+++
@@ -1,5 +0,0 @@
-placeholder :role_class do
-  match /(user|使用者|管理者)/ do
-    User
-  end
-end
--- spec/steps/sessions/login_steps.rb
+++ spec/steps/sessions/login_steps.rb
@@ -6,12 +6,12 @@ step ':model_finder 登入' do |role|
   signin_user(role) if role.is_a?(User)
 end
 
-step ':role_class 已登入' do |klass|
+step ':model_name 已登入' do |klass|
   get "/#{klass.to_s.underscore.pluralize}/sign_in"
   expect(response).to be_redirect
 end
 
-step ':role_class 未登入' do |klass|
+step ':model_name 未登入' do |klass|
   get "/#{klass.to_s.underscore.pluralize}/sign_in"
   expect(response).to be_success
 end
