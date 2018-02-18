7346ea4087fac2d69706f2a5ed8e615a9e9775e2
marsz <marsz330@gmail.com>
2018-02-18 15:53:21 +0800

    feat(locale): helpers for view translation and force raise while missing translate

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -59,10 +59,6 @@ Rails.application.configure do
 
   config.action_mailer.perform_caching = false
 
-  # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
-  # the I18n.default_locale when a translation cannot be found).
-  config.i18n.fallbacks = true
-
   # Send deprecation notices to registered listeners.
   config.active_support.deprecation = :notify
 
