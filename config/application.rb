da20f5b38248b1ef52c7c2c2ecb732a7d5778062
marsz <marsz330@gmail.com>
2019-06-22 18:32:00 +0800

    fix: rename to sqldesigner

--- config/application.rb
+++ config/application.rb
@@ -18,7 +18,7 @@ Bundler.require(*Rails.groups)
 
 Dotenv::Railtie.load
 
-module Myapp
+module Sqldesigner
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.2
--- db/schema.rb
+++ db/schema.rb
@@ -13,6 +13,7 @@
 ActiveRecord::Schema.define(version: 2019_06_22_053846) do
 
   # These are extensions that must be enabled in order to support this database
+  enable_extension "fuzzystrmatch"
   enable_extension "hstore"
   enable_extension "pg_trgm"
   enable_extension "plpgsql"
